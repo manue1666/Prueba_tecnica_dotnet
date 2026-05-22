1a7f342de8ef0bf2586d2d6b49b8ad0f25fbc81a
manue1666 <utm23090708@utma.edu.mx>
2026-05-21 23:24:51 -0600

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PruebaTecnica.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
